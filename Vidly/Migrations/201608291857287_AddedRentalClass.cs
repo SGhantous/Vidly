69f78b05c76d935910412160960a51cf54a08607
Steve Ghantous <steve@steveghantousyyc.com>
2016-08-29 14:01:18 -0600

    Implement sinmple NewRentals API that adds Rental objects to database.

---
+++ Vidly/Migrations/201608291857287_AddedRentalClass.cs
@@ -0,0 +1,16 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedRentalClass : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
